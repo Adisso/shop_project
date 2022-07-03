0954d687b3b7884e267343a057cd41a855fe9470
Adisso <adrian.pelechaty95@gmail.com>
2022-07-03 19:16:09 +0200

    Added Category Cards

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_project/size_config.dart';
+import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
 
@@ -16,26 +17,11 @@ class Body extends StatelessWidget {
             const HomeHeader(),
             SizedBox(height: getProportionateScreenWidth(30)),
             const DiscountBanner(),
-            Categories(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            const Categories(),
           ],
         ),
       ),
     );
-  }
-}
-
-class Categories extends StatelessWidget {
-  const Categories({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    List<Map<String, dynamic>> categories = [
-      {"icon": "assets/icons/Flash Icon.svg,", "text": "Flash Deal"},
-      {"icon": "assets/icons/Bill Icon.svg,", "text": "Bill"},
-      {"icon": "assets/icons/Game Icon.svg,", "text": "Game"},
-      {"icon": "assets/icons/Gift Icon.svg,", "text": "Daily Gift"},
-      {"icon": "assets/icons/Discover.svg,", "text": "More"},
-    ];
-    return SizedBox();
   }
 }
