37e3c8c45c80464bf3a4446997a7aba2f791133c
Adisso <adrian.pelechaty95@gmail.com>
2022-07-13 20:59:38 +0200

    Small Changes

--- lib/screens/sign_in/components/sing_form.dart
+++ lib/screens/sign_in/components/sing_form.dart
@@ -45,7 +45,7 @@ class _SignFormState extends State<SignForm> {
               const Text("Remember me"),
               const Spacer(),
               GestureDetector(
-                onTap: () => Navigator.popAndPushNamed(
+                onTap: () => Navigator.pushNamed(
                     context, ForgotPasswordScreen.routeName),
                 child: const Text(
                   "Forgot password?",
